8ab8237ea75d9b190cfc96623d6b15a6a8ef10d5
Noémi Villars-Amberg <117180458+Notheturtle@users.noreply.github.com>
2025-04-30 15:26:03 +0200

    Replace old function with ListLookup (#12)
    Co-authored-by: Johannes Nussbaum <johannes.nussbaum@dasch.swiss>

--- src/Scripts_xmllib/import_book_edition.py
+++ src/Scripts_xmllib/import_book_edition.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from dsp_tools.xmllib import (
     LicenseRecommended,
+    ListLookup,
     Resource,
-    create_label_to_name_list_node_mapping,
     create_list_from_string,
 )
 
@@ -19,10 +19,10 @@ def main() -> list[Resource]:
     book_df = pd.read_excel("data/Spreadsheet_Data/BookEdition.xlsx", dtype="str")
 
     # create list mapping
-    license_labels_to_names = create_label_to_name_list_node_mapping(
+    list_lookup = ListLookup.create_new(
         project_json_path=path_to_json,
-        list_name="License",
         language_of_label="en",
+        default_ontology="daschland",
     )
 
     # iterate through rows of dataframe:
@@ -30,7 +30,7 @@ def main() -> list[Resource]:
         # define variables
         book_path = f"{row['Directory']}{row['File Name']}"
         timestamp_value = get_media_file_creation_time(book_path)
-        license_name = license_labels_to_names[row["License List"]]
+        license_name = list_lookup.get_node_via_list_name(node_label=row["License List"], list_name="License")
         authors = create_list_from_string(string=row["Authorship"], separator=",")
 
         # create resource, label and id
