dcdbc49dfb443960c136173466af76343bfe176d
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-05-01 12:48:39 +0200

    edit

---
+++ src/excel2xml/import_book_cover_excel2xml.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from dsp_tools import excel2xml
+
+from src.helpers import helper_excel2xml
+
+
+def main():
+    all_resources = []
+
+    # define json file path
+    path_to_json = "daschland.json"
+
+    # create the root element dsp-tools
+    root = helper_excel2xml.make_root()
+
+    # define dataframe
+    book_df = pd.read_excel("data/spreadsheets/BookCover.xlsx", dtype="str")
+
+    # create list mapping
+    license_labels_to_names = excel2xml.create_json_list_mapping(
+        path_to_json=path_to_json,
+        list_name="License",
+        language_label="en",
+    )
+
+    # iterate through rows of dataframe:
+    for _, row in book_df.iterrows():
+        # define variables
+        resource_id = row["ID"]
+        resource_label = row["Label"]
+        uri = row["URI"]
+
+        # create resource, label and id
+        if not excel2xml.check_notna(row["ID"]):
+            continue
+
+        resource = excel2xml.make_resource(label=resource_label, restype=":BookCover", id=resource_id)
+
+        # add file to resource
+        resource.append(excel2xml.make_iiif_uri_prop(iiif_uri=uri))
+
+        # add properties to resource
+        if excel2xml.check_notna(row["ID"]):
+            resource.append(excel2xml.make_text_prop(":hasID", resource_id))
+        if excel2xml.check_notna(row["Description"]):
+            resource.append(
+                excel2xml.make_text_prop(
+                    ":hasDescription",
+                    excel2xml.PropertyElement(row["Description"], encoding="xml"),
+                )
+            )
+        if excel2xml.check_notna(row["Copyright"]):
+            resource.append(excel2xml.make_text_prop(":hasCopyright", row["Copyright"]))
+        if excel2xml.check_notna(row["License List"]):
+            license_name = license_labels_to_names[row["License List"]]
+            resource.append(excel2xml.make_list_prop("License", ":hasLicenseList", license_name))
+        if excel2xml.check_notna(row["Source"]):
+            resource.append(excel2xml.make_uri_prop(":hasUrl", row["Source"]))
+        if excel2xml.check_notna(row["Authorship"]):
+            authorship = [x.strip() for x in row["Authorship"].split(",")]
+            resource.append(excel2xml.make_text_prop(":hasAuthorship", authorship))
+
+        # append the resource to the list
+        all_resources.append(resource)
+
+    # add all resources to the root
+    root.extend(all_resources)
+
+    # write root to xml file
+    excel2xml.write_xml(root, "data/xml/import_book_cover.xml")
+    return all_resources
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/xmllib/import_documentation.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from dsp_tools.xmllib import (
+    LicenseRecommended,
+    ListLookup,
+    Resource,
+    create_list_from_string,
+)
+
+from src.helpers.image_helper import (
+    get_media_file_creation_time,
+    get_media_file_size,
+)
+
+
+def main() -> list[Resource]:
+    all_resources: list[Resource] = []
+
+    # define json file path
+    path_to_json = "daschland.json"
+
+    # define dataframe
+    documentation_df = pd.read_excel("data/spreadsheets/Documentation.xlsx", dtype="str")
+
+    # create list mapping
+    list_lookup = ListLookup.create_new(
+        project_json_path=path_to_json,
+        language_of_label="en",
+        default_ontology="daschland",
+    )
+    # iterate through rows of dataframe:
+    for _, row in documentation_df.iterrows():
+        # define variables
+        documentation_path = f"{row['Directory']}{row['File Name']}"
+        timestamp_value = get_media_file_creation_time(documentation_path)
+        file_size_value = get_media_file_size(documentation_path)
+        license_name = list_lookup.get_node_via_list_name(node_label=row["License List"], list_name="License")
+        authors = create_list_from_string(string=row["Authorship"], separator=",")
+
+        # create resource, label and id
+        resource = Resource.create_new(
+            res_id=row["ID"],
+            restype="metadata:Documentation",
+            label=row["File Name"],
+        )
+
+        # add file to resource
+        resource.add_file(
+            documentation_path, license=LicenseRecommended.CC.BY, copyright_holder=row["Copyright"], authorship=authors
+        )
+
+        # add properties to resource
+        resource.add_simpletext("metadata:hasID", row["ID"])
+        resource.add_richtext("metadata:hasDescription", row["Description"])
+        resource.add_simpletext("metadata:hasFileName", row["File Name"])
+        resource.add_time_optional("metadata:hasTimeStamp", timestamp_value)
+        resource.add_decimal_optional("metadata:hasFileSize", file_size_value)
+        resource.add_simpletext("metadata:hasCopyright", row["Copyright"])
+        resource.add_list("metadata:hasLicenseList", "License", license_name)
+        resource.add_simpletext_multiple("metadata:hasAuthorship", row["Authorship"])
+
+        # append resource to list
+        all_resources.append(resource)
+
+    return all_resources
+
+
+if __name__ == "__main__":
+    main()
