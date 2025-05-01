be5c6f3e82e55d3e79ee2ef5b35b763180bc6168
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-05-01 12:54:22 +0200

    no uppercase letters in src directory (#19)

---
+++ src/excel2xml/import_image_region_excel2xml.py
@@ -0,0 +1,67 @@
+import json
+
+import pandas as pd
+from dsp_tools import excel2xml
+
+from src.helpers import helper_excel2xml
+
+
+def main():
+    all_resources = []
+
+    # create the root element dsp-tools
+    root = helper_excel2xml.make_root()
+
+    # define dataframe
+    archive_df = pd.read_excel("data/spreadsheets/Region.xlsx")
+
+    # iterate through rows of dataframe:
+    for _, row in archive_df.iterrows():
+        # define variables
+        region_id = row["ID"]
+        region_label = row["Comment"]
+        color = row["Color"]
+        x1 = row["Geometry X1"]
+        y1 = row["Geometry Y1"]
+        x2 = row["Geometry X2"]
+        y2 = row["Geometry Y2"]
+        geometry = {
+            "status": "active",
+            "type": "rectangle",
+            "lineColor": color,
+            "lineWidth": 5,
+            "points": [{"x": x1, "y": y1}, {"x": x2, "y": y2}],
+        }
+        JSON_string = json.dumps(geometry)
+
+        # create region, label and id
+        if not excel2xml.check_notna(row["ID"]):
+            continue
+
+        region = excel2xml.make_region(label=region_label, id=region_id)
+
+        # add properties to resource
+        if excel2xml.check_notna(row["Color"]):
+            region.append(excel2xml.make_color_prop("hasColor", row["Color"]))
+        if excel2xml.check_notna(row["Image ID"]):
+            region.append(excel2xml.make_resptr_prop("isRegionOf", row["Image ID"]))
+        if excel2xml.check_notna(row["Geometry X1"]):
+            region.append(excel2xml.make_geometry_prop("hasGeometry", JSON_string))
+        if excel2xml.check_notna(row["Comment"]):
+            region.append(
+                excel2xml.make_text_prop("hasComment", excel2xml.PropertyElement(value=row["Comment"], encoding="xml"))
+            )
+
+        # append the region to the list
+        all_resources.append(region)
+
+    # add all resources to the root
+    root.extend(all_resources)
+
+    # write root to xml file
+    excel2xml.write_xml(root, "data/xml/import_region.xml")
+    return all_resources
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/xmllib/import_video.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from dsp_tools.xmllib import (
+    LicenseRecommended,
+    ListLookup,
+    Resource,
+    create_list_from_string,
+)
+
+from src.helpers.image_helper import (
+    get_media_file_creation_time,
+    get_media_file_size,
+)
+
+
+def main() -> list[Resource]:
+    all_resources: list[Resource] = []
+
+    # define json file path
+    path_to_json = "daschland.json"
+
+    # define dataframe
+    video_df = pd.read_excel("data/spreadsheets/Video.xlsx", dtype="str")
+
+    # create list mapping
+    list_lookup = ListLookup.create_new(
+        project_json_path=path_to_json,
+        language_of_label="en",
+        default_ontology="daschland",
+    )
+
+    # iterate through rows of dataframe:
+    for _, row in video_df.iterrows():
+        # define variables
+        video_path = f"{row['Directory']}{row['File Name']}"
+        timestamp_value = get_media_file_creation_time(video_path)
+        file_size_value = get_media_file_size(video_path)
+        license_name = list_lookup.get_node_via_list_name(node_label=row["License List"], list_name="License")
+        authors = create_list_from_string(string=row["Authorship"], separator=",")
+
+        # create resource, label and id
+        resource = Resource.create_new(
+            res_id=row["ID"],
+            restype=":Video",
+            label=row["Label"],
+        )
+
+        # add file to resource
+        resource.add_file(
+            video_path,
+            license=LicenseRecommended.DSP.PUBLIC_DOMAIN,
+            copyright_holder=row["Copyright"],
+            authorship=authors,
+        )
+
+        # add properties to resource
+        resource.add_simpletext(":hasID", row["ID"])
+        resource.add_time_optional(":hasTimeStamp", timestamp_value)
+        resource.add_decimal_optional(":hasFileSize", file_size_value)
+        resource.add_simpletext(":hasCopyright", row["Copyright"])
+        resource.add_list(":hasLicenseList", "License", license_name)
+        resource.add_simpletext(":hasFileName", row["File Name"])
+        resource.add_richtext(":hasDescription", row["Description"])
+        resource.add_richtext(":hasCast", row["Cast"])
+        resource.add_simpletext_multiple(":hasAuthorship", row["Authorship"])
+
+        # append resource to list
+        all_resources.append(resource)
+
+    return all_resources
+
+
+if __name__ == "__main__":
+    main()
