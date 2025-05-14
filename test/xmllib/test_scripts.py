0434a371af4071a67cf4039970136a2a943c80bf
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-05-14 08:55:23 +0200

    when running tests, don't print XmllibInputInfo warnings (#29)

--- test/xmllib/test_scripts.py
+++ test/xmllib/test_scripts.py
@@ -1,5 +1,8 @@
+import pytest
+
 from src.xmllib.main import main
 
 
+@pytest.mark.filterwarnings("ignore::dsp_tools.error.xmllib_warnings.XmllibInputInfo")
 def test_scripts() -> None:
     main()
