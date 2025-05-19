9be1ebaa7a2e6d59b79919f2a2cd4698650684d8
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-05-19 16:34:52 +0200

    make XML file consistent across runs (#34)

--- test/xmllib/test_scripts.py
+++ test/xmllib/test_scripts.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from src.xmllib.main import main
@@ -5,4 +7,7 @@ from src.xmllib.main import main
 
 @pytest.mark.filterwarnings("ignore::dsp_tools.error.xmllib_warnings.XmllibInputInfo")
 def test_scripts() -> None:
+    os.environ["XMLLIB_SORT_RESOURCES"] = "true"
+    os.environ["XMLLIB_SORT_PROPERTIES"] = "true"
+    os.environ["XMLLIB_AUTHORSHIP_ID_WITH_INTEGERS"] = "true"
     main()
