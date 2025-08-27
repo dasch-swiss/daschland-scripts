8667b4b66b6aa1640c2170906ad59102eeb034ef
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-08-27 16:51:01 +0200

    just format

--- src/xmllib/import_link_object.py
+++ src/xmllib/import_link_object.py
@@ -6,7 +6,6 @@ from dsp_tools.xmllib import (
 
 
 def main() -> list[LinkResource]:
-
     all_link_resources: list[LinkResource] = []
 
     # define dataframe
@@ -14,13 +13,9 @@ def main() -> list[LinkResource]:
 
     # iterate through rows of dataframe:
     for _, row in link_df.iterrows():
-
         # define variables
         links = create_list_from_input(input_value=row["Links"], separator=",")
-        link_resource = LinkResource.create_new(
-            res_id=row["ID"],
-            label=row["Label"],
-            link_to=links)
+        link_resource = LinkResource.create_new(res_id=row["ID"], label=row["Label"], link_to=links)
 
         link_resource = link_resource.add_comment(text=row["Text"], comment=row["Comment"])
 
