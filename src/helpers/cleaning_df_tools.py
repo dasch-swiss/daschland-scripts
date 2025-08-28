01b0240608e9441618c61b8a8d2ed0183a4e17eb
Noémi Villars-Amberg <117180458+Notheturtle@users.noreply.github.com>
2025-08-28 09:23:03 +0200

    delete unused function

--- src/helpers/cleaning_df_tools.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-import pandas as pd
-
-
-def create_list(input_value: Any) -> list[str]:
-    if pd.isna(input_value):
-        return []
-    return [x.strip() for x in input_value.split(",")]
