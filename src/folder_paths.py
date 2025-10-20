b9172f1940606a4726332d20c233ca1c66677057
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-20 09:47:17 +0200

    Create folder_paths.py

--- src/folder_paths.py
+++ src/folder_paths.py
@@ -2,4 +2,7 @@ from pathlib import Path
 
 INPUT_FOLDER = Path("data")
 
+MULTIMEDIA_FOLDER = INPUT_FOLDER / "multimedia"
+NODEGOAT_FOLDER = INPUT_FOLDER / "nodegoat"
 SPREADSHEETS_FOLDER = INPUT_FOLDER / "spreadsheets"
+XML_FOLDER = INPUT_FOLDER / "xml"
