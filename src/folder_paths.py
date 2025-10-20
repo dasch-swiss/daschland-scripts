e0dbe063efc2fd35f638c9597536912c2e9ffe0d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-20 13:04:53 +0200

    Commit all changes

--- src/folder_paths.py
+++ src/folder_paths.py
@@ -2,6 +2,4 @@ from pathlib import Path
 
 INPUT_FOLDER = Path("data")
 
-MULTIMEDIA_FOLDER = INPUT_FOLDER / "multimedia"
 SPREADSHEETS_FOLDER = INPUT_FOLDER / "spreadsheets"
-XML_FOLDER = INPUT_FOLDER / "xml"
