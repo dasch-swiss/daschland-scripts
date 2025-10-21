7dbfaf8b95b7b4f24e5204ee6c769ebddaa34168
Noémi Villars-Amberg <117180458+Notheturtle@users.noreply.github.com>
2025-10-21 16:58:21 +0200

    minor changes

--- test/xmllib/test_scripts.py
+++ test/xmllib/test_scripts.py
@@ -11,7 +11,7 @@ from dsp_tools.error.xmllib_warnings import XmllibInputInfo
 
 from src.xmllib.xmllib_main import main
 
-XML_FILE = Path("data_daschland.xml")
+XML_FILE = Path("data/output/data_daschland.xml")
 ENV_VARS = ["XMLLIB_SORT_RESOURCES", "XMLLIB_SORT_PROPERTIES", "XMLLIB_AUTHORSHIP_ID_WITH_INTEGERS"]
 
 
