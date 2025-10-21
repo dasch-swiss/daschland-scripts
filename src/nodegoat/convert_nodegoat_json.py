3139535e71a9c264bc4aa7930ca95d385684241b
Noémi Villars-Amberg <117180458+Notheturtle@users.noreply.github.com>
2025-10-21 13:51:19 +0200

    nodegoat restructuration 1

--- src/nodegoat/convert_nodegoat_json.py
+++ src/nodegoat/convert_nodegoat_json.py
@@ -12,7 +12,7 @@ def transform_to_daschland(nodegoat_data: dict[str, Any]) -> dict[str, Any]:
 
     # Example: Mapping the nodegoat 'Book' type to daschland 'Book' structure
     for type_name, type_data in nodegoat_data["data"]["types"].items():
-        if type_name == "Book":  # Example for Book, add similar transformations for other types
+        if type_name == "Book":
             transformed_data["book"] = {
                 "name": type_data["type"]["name"],
                 "description": type_data["object_descriptions"]["Description"]["object_description_name"],
